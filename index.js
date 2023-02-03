b03304d50841da3c96b4a2ec9fe2bc01747bbaaa
vinney-mo <vinneymotanya@gmail.com>
2023-02-03 15:27:36 +0300

    Added Mongoose Connection

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express();
 
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
+
+//Connect app to MongoDB
+const connectionString = process.env.DATABASE_URL
+mongoose.connect(connectionString);
+
+const database = mongoose.connection;
+
+database.on('error', (error)=>{
+    console.log(error);
+});
+
+database.on('connected', ()=>{
+    console.log("Database Connected");
+});
 
 // CRUD OPERATIONS
 
