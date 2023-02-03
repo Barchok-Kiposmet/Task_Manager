9dcca2c6dd3c774e2d1de809e9a0bb69620fb0bd
Barchok-Kiposmet <barchokpatrick@gmail.com>
2023-02-03 22:55:52 +0300

    Added get request

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,28 +1,29 @@
-const express = require('express');
-const tasksModels =require('./../models/tasks');
+const express = require("express");
+const tasksModels = require("./../models/tasks");
 
 const router = express.Router();
 
-router.post('/', async function (req, res) {
-      console.log(req.body)
-    const data = new tasksModels({
-      title: req.body.title,
-      description: req.body.description
-    });
-
-    try { 
-      const dataToSave = await data.save();
-      res.status(200).json(dataToSave);
-      
-    } catch (error) {
-    res.status(400).json({ message:error.message});
-    }
+router.post("/", async function (req, res) {
+  const data = new tasksModels({
+    title: req.body.title,
+    description: req.body.description,
   });
 
-  router.get('/', function (req, res) {
-    res.send('Hello World')
-  });
+  try {
+    const dataToSave = await data.save();
+    res.status(200).json(dataToSave);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 
+router.get("/", async function (req, res) {
+  try {
+    const data = await tasksModels.find();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 
-
-  module.exports = router;
+module.exports = router;
